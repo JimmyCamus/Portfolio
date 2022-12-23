8ebad7296412948c33e55ba6f392e753c77f74a5
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-23 16:39:27 -0300

    CI: tailwind theme was added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#6DD5ED",
+      secondary: "#CC2B5E",
+      "primary-gradient": "#2193B0",
+      "secondary-gradient": "#753A88",
+      white: "#FFFFFF",
+      black: "#111111",
+      "neutral-content": "#666666",
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
