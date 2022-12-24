d22504e8befc3676d4d2ee907d3e77aa4d400a16
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-24 12:42:31 -0300

    feat: socials media section was created

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import GlowContainer from "../containers/Glow.container";
 import ArrowDown from "./icons/ArrowDown";
+import SocialMedia from "./SocialMedia";
 
 const Hero = () => {
   return (
-    <div className="hero min-h-screen bg-white pb-24">
+    <div className="hero min-h-screen bg-white">
       <div className="hero-content flex-col">
         <Image
           src={process.env.FIREBASE_PERSONAL_IMAGE || ""}
@@ -20,7 +21,10 @@ const Hero = () => {
           <h2 className="font-extrabold mt-2 text-2xl text-transparent bg-clip-text bg-gradient-to-r from-secondary to-secondary-gradient text-center">
             Desarrollador Fullstack
           </h2>
-          <div className="flex items-center mt-20 text-center">
+          <div className="mt-7">
+            <SocialMedia />
+          </div>
+          <div className="flex items-center mt-10 text-center">
             <GlowContainer from="primary-gradient" to="primary">
               <ArrowDown />
             </GlowContainer>
