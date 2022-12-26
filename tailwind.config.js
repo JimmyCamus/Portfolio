f7956d0e4e211150e174759bc854e108ad70ea47
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-26 18:37:56 -0300

    CI: new color was added to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,10 @@ module.exports = {
     colors: {
       primary: "#6DD5ED",
       secondary: "#CC2B5E",
+      tertiary: "#FF9966",
       "primary-gradient": "#2193B0",
       "secondary-gradient": "#753A88",
+      "tertiary-gradient": "#FF5E62",
       white: "#FFFFFF",
       black: "#111111",
       "neutral-content": "#666666",
