f7d661a22042bc1441b98a813a0de3b78505ab7b
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-27 16:58:13 -0300

    feat: section label component was added to about and project section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,21 +1,30 @@
 import { Project } from "../lib/types/project.type";
 import Dev from "./icons/Dev";
 import ProjectList from "./Projects/ProjectList";
+import SectionLabel from "./SectionLabel";
 import SectionTitle from "./SectionTitle";
 
 const Projects = ({ projects }: { projects: Project[] }) => {
   return (
-    <div className="mt-20 flex flex-col items-center justify-center">
-      <div className="mb-5">
-        <SectionTitle gradientFrom="secondary" gradientTo="secondary-gradient">
-          PROJECTS
-        </SectionTitle>
-      </div>
-      <div className="flex items-center flex-col">
-        <div className="mb-4">
-          <Dev />
+    <div className="mt-20 lg:mt-60">
+      <SectionLabel gradientFrom="secondary" gradientTo="secondary-gradient">
+        2
+      </SectionLabel>
+      <div className="flex flex-col items-center justify-center">
+        <div className="mb-5">
+          <SectionTitle
+            gradientFrom="secondary"
+            gradientTo="secondary-gradient"
+          >
+            PROJECTS
+          </SectionTitle>
         </div>
-        <ProjectList projects={projects} />
+        <div className="flex items-center flex-col">
+          <div className="mb-4">
+            <Dev />
+          </div>
+          <ProjectList projects={projects} />
+        </div>
       </div>
     </div>
   );
